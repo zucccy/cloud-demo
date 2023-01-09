afdd24ccd4cd78450b87e4fb023f078c4a788d81
seucy <1007677685@qq.com>
2023-01-09 10:50:11 +0800

    2023/1/9 first commit

---
+++ cloud-demo/eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description: TODO
+ * @Author: ChenYun
+ * @CreateDate: 2023/1/3 17:01
+ * @Version 1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
---
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    // 先将RestTemplate注册进来
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2.使用RestTemplate进行远程调用获取用户信息
+        // 2.1.自定义url路径
+        String url = "http://userservice/user/" + order.getUserId();
+//        String url = "http://localhost:8081/user/" + order.getUserId();
+        // 参数中有个responseType，是Class<T>类型，是为了将返回的json反序列化成需要的class对象，这里写User.class
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
